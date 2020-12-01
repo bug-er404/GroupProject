3992e5bcbf6ca2d4126db5c17ca4a735e4077642
Yue Yang <yueyang@yues-mbp.myfiosgateway.com>
2020-12-01 15:04:06 -0500

    Initial commit

---
+++ src/main/java/com/group/groupoproject/GroupoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.group.groupoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupoprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupoprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/groupoproject/GroupoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.groupoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupoprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
